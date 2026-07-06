01621ba93e991902bf3c45c5d7782e204ad17b63
Meet Bhut <meetbhut321@gmail.com>
2026-07-06 21:25:15 +0530

    Time: 5 ms (64.70%) | Memory: 12.4 MB (64.20%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        res = []
+        depth = 0
+        for ch in s:
+            if ch == '(':
+                if depth > 0:
+                    res.append(ch)
+                depth += 1
+            else:
+                depth -= 1
+                if depth > 0:
+                    res.append(ch)
+        return "".join(res)
