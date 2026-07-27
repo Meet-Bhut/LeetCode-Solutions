d37069cba429f9490085041e5bf531131a0c103c
Meet Bhut <meetbhut321@gmail.com>
2026-07-27 12:19:07 +0530

    Time: 4 ms (19.47%) | Memory: 12.4 MB (50.33%) - LeetSync

---
+++ 4332-check-good-integer/check-good-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def checkGoodInteger(self, n):
+        dsum=0
+        ssum=0
+        while (n>0):
+            digit=n%10
+            dsum+=digit
+            ssum+=digit*digit
+            n/=10
+
+        if (ssum-dsum>=50):
+            return True
+        else:
+            return False
+
+        
